e5f1fb200561f4a40761d4517e1af84f8e924356
hanii-patel <drjkp7535@gmail.com>
2026-04-27 17:37:43 +0530

    Add prompt UI and template for research tool

---
+++ prompt_ui.py
@@ -0,0 +1,51 @@
+from langchain_core.prompts import load_prompt
+from langchain_ollama import OllamaLLM
+import streamlit as st
+from langchain_core.prompts import PromptTemplate,load_prompt
+import os
+
+
+model = OllamaLLM(
+    model="llama3",
+    temperature=0.5
+)
+st.header('Research Tool')
+
+paper_input = st.selectbox( 
+    "Select Research Paper Name", 
+    ["Attention Is All You Need", 
+    "BERT: Pre-training of Deep Bidirectional Transformers", 
+    "GPT-3: Language Models are Few-Shot Learners", 
+    "Diffusion Models Beat GANs on Image Synthesis"
+    ] 
+)
+
+style_input = st.selectbox( 
+    "Select Explanation Style", 
+    ["Beginner-Friendly", 
+    "Technical",
+    "Code-Oriented", 
+    "Mathematical"
+    ] 
+) 
+
+length_input = st.selectbox( 
+    "Select Explanation Length", 
+    ["Short (1-2 paragraphs)", 
+    "Medium (3-5 paragraphs)", 
+    "Long (detailed explanation)"
+    ] 
+)
+
+template = load_prompt('template.json')
+
+
+
+if st.button('Summarize'):
+    chain = template | model
+    result = chain.invoke({
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
+    })
+    st.write(result)
