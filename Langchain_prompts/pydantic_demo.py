81ebe9979999f6d383d3fb1a81544ada492e5c53
hanii-patel <drjkp7535@gmail.com>
2026-04-30 12:50:20 +0530

    Add structured output examples with JSON and Pydantic

--- Langchain_prompts/pydantic_demo.py
+++ Langchain_prompts/pydantic_demo.py
@@ -1,4 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel , EmailStr, Field
+from typing import Optional
 
+#Basic Example
+class Student(BaseModel):
+    name : str
+    age : int
 
+#Default Input
+class Student(BaseModel):
+    name : str = 'Yash'
+    age : int = 20
+
+#Optional Field , Field Functions
+class Student(BaseModel):
+    name : str = 'Meet'
+    age : Optional[int] = None
+    email : EmailStr
+    cgpa : float = Field (gt=0 , lt=10)
+
+new_student = Student(name='Hani' , age=22 , email='hani@gmail.com' , cgpa=9.5)
+print(new_student)
 
